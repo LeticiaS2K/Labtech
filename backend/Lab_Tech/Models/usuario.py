629d7b903df94d254584778b3877fb30c66f6643
luisssh <lhlds78@gmail.com>
2025-11-28 23:35:12 -0300

    Adiciona backend com projeto Lab_Tech

---
+++ backend/Lab_Tech/Models/usuario.py
@@ -0,0 +1,20 @@
+from database import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    senha_hash = db.Column(db.String(200), nullable=False)
+    criado_em = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_senha(self, senha):
+        self.senha_hash = generate_password_hash(senha)
+
+    def checar_senha(self, senha):
+        return check_password_hash(self.senha_hash, senha)
+
+    def __repr__(self):
+        return f"<Usuario {self.email}>"
